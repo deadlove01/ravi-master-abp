3e95f795a1a1361f22c5f8d1e54870f3c8a02b73
Ravi Le <ravi.le@soxes.ch>
2023-08-06 02:55:55 +0700

    add Product app service and test

---
+++ test/ProductManagement.Application.Tests/Products/ProductAppServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Shouldly;
+using Volo.Abp.Application.Dtos;
+using Xunit;
+
+namespace ProductManagement.Products;
+
+public class ProductAppServiceTests: ProductManagementApplicationTestBase
+{
+    private readonly IProductAppService _productAppService;
+
+    public ProductAppServiceTests()
+    {
+        _productAppService = GetRequiredService<IProductAppService>();
+    }
+    
+    [Fact]
+    public async Task Should_Get_List_Of_Products()
+    {
+        //Act
+        var result = await _productAppService.GetListAsync(new PagedAndSortedResultRequestDto());
+
+        //Assert
+        result.TotalCount.ShouldBeGreaterThan(0);
+        //Acme Monochrome Laser Printer, Compact All-In One
+        result.Items.ShouldContain(p => p.Name.Contains("Acme Monochrome Laser Printer"));
+    }
+}
